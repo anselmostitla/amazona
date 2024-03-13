b495b520f7fafd34251bad1fee6d9b85f098fa04
anselmo <anselmostitla@gmail.com>
2024-03-12 22:04:49 -0600

    defining product types and data and rendering images

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-  <div>
-    <header>Amazona</header>
-    <main>This a hero</main>
-    <footer>facebook</footer>
-  </div>
+    <div>
+      <header>Amazona</header>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img src={product.image} alt={product.name} className='product-image'/>
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
+      <footer>All rights reserved</footer>
+    </div>
   )
 }
 
