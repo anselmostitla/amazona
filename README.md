4dd53935e94ef18e68e378fc03688f32ebc618e4
anselmo <anselmostitla@gmail.com>
2024-03-12 20:54:21 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Typescript MERN Amazona
+
+## Lessons
+
+1. Introduction
+2. Install Tools
+3. Create Typescript React App By Vite
+4. Create Git Repository
---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <div>
+    <header>Amazona</header>
+    <main>This a hero</main>
+    <footer>facebook</footer>
+  </div>
+  )
+}
+
+export default App
